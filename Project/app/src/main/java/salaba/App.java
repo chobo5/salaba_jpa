51f29b3f01f5deb5b282bab7db6cfb4c16512b49
JUNYEON WON <wnsdus1008@gmail.com>
2024-09-12 15:56:14 +0900

    설정

--- Project/app/src/main/java/salaba/App.java
+++ Project/app/src/main/java/salaba/App.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import salaba.domain.common.service.InitService;
 
@@ -21,6 +22,10 @@ import javax.persistence.EntityManager;
 @RequiredArgsConstructor
 @EnableCaching
 @EnableAspectJAutoProxy
+@PropertySource({
+        "file:${user.home}/config/jdbc.properties",
+        "file:${user.home}/config/jwt.properties"
+})
 public class App {
     private final InitService initService;
 
